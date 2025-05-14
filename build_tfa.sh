c362eac157769c39198b12bda1138104fb3821f2
Ajay Iyengar <12970408+ajiyengar@users.noreply.github.com>
2025-05-14 05:28:42 -0700

    build(tfa): add qcbor for RME support

--- build_tfa.sh
+++ build_tfa.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-# TF-A
-#make -C trusted-firmware-a \
-#  CROSS_COMPILE=aarch64-none-elf- \
-#  OPENSSL_DIR="${HOMEBREW_PREFIX}"/opt/openssl \
-#  PLAT=qemu_sbsa \
-#  DEBUG=1 \
-#  all fip
-#
-#cp trusted-firmware-a/build/qemu_sbsa/debug/bl1.bin edk2-non-osi/Platform/Qemu/Sbsa/
-#cp trusted-firmware-a/build/qemu_sbsa/debug/fip.bin edk2-non-osi/Platform/Qemu/Sbsa/
+# Get the directory of the currently running script
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-# UEFI
-export PACKAGES_PATH=./edk2:./edk2-platforms:./edk2-non-osi
-make -C edk2/BaseTools
-. edk2/edksetup.sh
+# Build TF-A
+make -C trusted-firmware-a \
+  CROSS_COMPILE=aarch64-none-elf- \
+  OPENSSL_DIR="${HOMEBREW_PREFIX}"/opt/openssl \
+  QCBOR_DIR="${SCRIPT_DIR}"/qcbor \
+  PLAT=qemu_sbsa \
+  DEBUG=1 \
+  all fip
 
-build -b DEBUG -a AARCH64 -t GCC5 -p edk2-platforms/Platform/Qemu/SbsaQemu/SbsaQemu.dsc
+# Copy build artifacts to edk2-non-osi
+cp trusted-firmware-a/build/qemu_sbsa/debug/bl1.bin edk2-non-osi/Platform/Qemu/Sbsa/
+cp trusted-firmware-a/build/qemu_sbsa/debug/fip.bin edk2-non-osi/Platform/Qemu/Sbsa/
